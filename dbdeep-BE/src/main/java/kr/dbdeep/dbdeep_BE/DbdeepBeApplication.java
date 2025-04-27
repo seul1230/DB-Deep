c2f406868b21ce44593548f07b89b54626d78ed7
kjh-0523 <nemo00528@gmail.com>
2025-04-27 16:49:44 +0900

    init : 개발 환경 설정

---
+++ dbdeep-BE/src/main/java/kr/dbdeep/dbdeep_BE/DbdeepBeApplication.java
@@ -0,0 +1,13 @@
+package kr.dbdeep.dbdeep_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbdeepBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbdeepBeApplication.class, args);
+	}
+
+}
---
+++ dbdeep-BE/src/test/java/kr/dbdeep/dbdeep_BE/DbdeepBeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.dbdeep.dbdeep_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbdeepBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
