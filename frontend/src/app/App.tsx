292b1d8d60d8351c782ff52eaebb03ce1dbb7b95
오준수 <ojs9551@naver.com>
2025-04-29 17:29:18 +0900

    feat:메인화면 라우팅 테스트

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,13 +1,20 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import LoginPage from "@/pages/LoginPage/LoginPage";
+
+// MainPage는 Lazy 로딩
+const MainPage = lazy(() => import("@/pages/MainPage/MainPage"));
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Suspense fallback={<div>페이지 로딩 중...</div>}>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/main" element={<MainPage />} />
       </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
---
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MainPage: React.FC = () => {
+  return (
+    <div className="mainpage-container">
+      <h1 className="mainpage-title">메인 페이지</h1>
+      <p className="mainpage-description">로그인에 성공했습니다. 환영합니다!</p>
+    </div>
+  );
+};
+
+export default MainPage;
