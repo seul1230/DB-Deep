043d4bae5663ceb53995a0a47f09671a4cf38227
이승우 <swoolee97@ajou.ac.kr>
2025-05-08 13:47:20 +0900

    S12P31S104-98 [BE] 부서 필터링 검색 API 구현

--- dbdeep-BE/src/main/java/kr/dbdeep/dbdeep_BE/domain/member/api/MemberController.java
+++ dbdeep-BE/src/main/java/kr/dbdeep/dbdeep_BE/domain/member/api/MemberController.java
@@ -8,6 +8,7 @@ import kr.dbdeep.dbdeep_BE.global.response.JSONResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +22,14 @@ public class MemberController {
     public JSONResponse<?> findAllMembers() {
         List<MemberDto> members = memberService.findAll();
         return JSONResponse.onSuccess(MemberInfoResponse.builder().members(members).build());
+    }
+
+    @GetMapping("/search")
+    public JSONResponse<?> search(@RequestParam(required = false) String name,
+                                  @RequestParam(required = false) String email,
+                                  @RequestParam(required = false) String teamName) {
+        List<MemberDto> members = memberService.search(name, email, teamName);
+        return JSONResponse.onSuccess(members);
     }
 
 }
