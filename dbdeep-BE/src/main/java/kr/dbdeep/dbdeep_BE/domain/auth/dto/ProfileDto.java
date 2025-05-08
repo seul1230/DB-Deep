9420e6ea8175cf80f0dd143a13bc7b3bf765b9c6
이승우 <swoolee97@ajou.ac.kr>
2025-05-08 22:24:33 +0900

    add: ProfileDto에 이름, 부서이름 추가

--- dbdeep-BE/src/main/java/kr/dbdeep/dbdeep_BE/domain/auth/dto/ProfileDto.java
+++ dbdeep-BE/src/main/java/kr/dbdeep/dbdeep_BE/domain/auth/dto/ProfileDto.java
@@ -5,15 +5,19 @@ import lombok.Builder;
 
 @Builder
 public record ProfileDto(
+        String name,
         String email,
         String imageUrl,
+        String teamName,
         Boolean passwordNotChanged
 ) {
 
     public static ProfileDto from(Member member) {
         return ProfileDto.builder()
                 .email(member.getEmail())
+                .name(member.getName())
                 .imageUrl(member.getProfileImage())
+                .teamName(member.getDepartment().getName())
                 .passwordNotChanged(member.getPasswordNotChanged())
                 .build();
     }
