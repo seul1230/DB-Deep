61b656f310772b6df9911bbaca43540ce1e3efa7
이승우 <swoolee97@ajou.ac.kr>
2025-05-08 22:28:33 +0900

    add: NotificationResponse에 chatId 추가

--- dbdeep-BE/src/main/java/kr/dbdeep/dbdeep_BE/domain/notification/api/dto/NotificationResponse.java
+++ dbdeep-BE/src/main/java/kr/dbdeep/dbdeep_BE/domain/notification/api/dto/NotificationResponse.java
@@ -2,21 +2,25 @@ package kr.dbdeep.dbdeep_BE.domain.notification.api.dto;
 
 import java.time.LocalDateTime;
 import kr.dbdeep.dbdeep_BE.domain.notification.entity.Notification;
+import lombok.Builder;
 
+@Builder
 public record NotificationResponse(
         Integer id,
         String content,
         Boolean isRead,
+        String chatId,
         LocalDateTime readAt,
         LocalDateTime createdAt
 ) {
     public static NotificationResponse from(Notification notification) {
-        return new NotificationResponse(
-                notification.getId(),
-                notification.getContent(),
-                notification.getIsRead(),
-                notification.getReadAt(),
-                notification.getCreatedAt()
-        );
+        return NotificationResponse.builder().
+                id(notification.getId())
+                .content(notification.getContent())
+                .isRead(notification.getIsRead())
+                .chatId(notification.getChatroomId())
+                .readAt(notification.getReadAt())
+                .createdAt(notification.getCreatedAt())
+                .build();
     }
 }
