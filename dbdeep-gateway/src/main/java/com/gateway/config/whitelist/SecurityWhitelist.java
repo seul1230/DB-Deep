d3f8e6596de1206a44f541e269c77b435daab087
김지호 <nemo00528@gmail.com>
2025-05-13 13:05:26 +0900

    fix : swagger 설정 추가

--- dbdeep-gateway/src/main/java/com/gateway/config/whitelist/SecurityWhitelist.java
+++ dbdeep-gateway/src/main/java/com/gateway/config/whitelist/SecurityWhitelist.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class SecurityWhitelist {
     public static final List<String> PERMIT_ALL = List.of(
             "/auth/**",
+            "/swagger-ui.html",
             "/swagger-ui/**",
             "/v3/api-docs/**",
             "/swagger-resources/**"
