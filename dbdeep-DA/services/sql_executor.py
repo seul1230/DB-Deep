b0398dab02b6586d1d273ed0cc7ea8ba5689a7fb
cupwan <pkwmj44@gmail.com>
2025-05-14 11:07:43 +0900

    fix: sql_executor env 수정

--- dbdeep-DA/services/sql_executor.py
+++ dbdeep-DA/services/sql_executor.py
@@ -6,11 +6,15 @@ from google.oauth2 import service_account
 
 class SQLExecutor:
     def __init__(self):
-        credentials = service_account.Credentials.from_service_account_file(settings.GOOGLE_APPLICATION_CREDENTIALS)
-        if credentials == None:
-            self.bq_client = bigquery.Client(project=settings.GOOGLE_CLOUD_PROJECT)
-        else:
-            self.bq_client = bigquery.Client(credentials=credentials, project=settings.GOOGLE_CLOUD_PROJECT)
+
+        credentials_path = settings.GOOGLE_APPLICATION_CREDENTIALS
+
+        if credentials_path:
+            if os.path.exists(credentials_path):
+                credentials = service_account.Credentials.from_service_account_file(credentials_path)
+                self.bq_client = bigquery.Client(credentials=credentials, project=settings.GOOGLE_CLOUD_PROJECT)
+            else:
+                self.bq_client = bigquery.Client(project=settings.GOOGLE_CLOUD_PROJECT)
 
     def validate(self, query: str, location: str = "asia-northeast3"):
         job_config = bigquery.QueryJobConfig(dry_run=True, use_query_cache=False)
